c08f665e30f05886c716031c85e78c40826945e4
lupengyu <leonlu@outlook.com>
2016-12-21 17:13:59 +0800

    使用不合并版本

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,16 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const clean = require('gulp-clean');
-const babelHelpers = require('gulp-babel-external-helpers');
-
 const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('babel', function () {
     return gulp.src('src/**/*.js')
         .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(babelHelpers('babelHelpers.js', 'umd'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('lib'));
 });
 
 gulp.task('build', ['babel']);
 
-gulp.task('clean', function () {
-    return gulp
-        .src('dist', {read: false})
-        .pipe(clean());
-});
-
-gulp.task('rebuild', ['clean', 'build']);
+gulp.task('default', ['build']);
